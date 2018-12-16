b172eb98ca47f6507ee94479dc30b093aac00a00
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-12-16 10:31:04 +0700

    Add navigation drawer

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
       backgroundColor: Colors.yellowAccent,
       appBar: AppBar(
         title: Text('Hello World Flutter'),
-        leading: Icon(
-          Icons.menu
-        ),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.shopping_cart),
@@ -20,6 +18,25 @@ void main() {
           )
         ],
       ),
-    )
+      body: Container(
+        color: Colors.red,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              child: Text("Drawer Header"),
+              decoration: BoxDecoration(color: Colors.lightBlue),
+            ),
+            Text("Item 1"),
+            Text("Item 2"),
+            Text("Item 3"),
+            Text("Item 4"),
+            Text("Item 5"),
+            Text("Item 6")
+          ],
+        ),
+      ),
+    ),
   ));
 }
